26bd90e8e79d488d517382424b077c6061312b57
Rahul Shenoy <Rahul_Shenoy@amat.com>
2018-04-25 02:43:15 +0530

    Style changes

--- src/body.js
+++ src/body.js
@@ -2,7 +2,7 @@ import { Reader } from './contextwrapper'
 import React from 'react'
 export default function(){
     return (
-        <ul>
+        <ul className="changelog">
             <Reader>
                 {context => context.body.changelog.map((change)=><li key={change.ts}>{change.text}</li>)}
             </Reader>
